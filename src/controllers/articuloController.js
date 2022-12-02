bc368bd95835b3319d3e0bbfc30cd44947a54330
Alejandro Urbina Suárez <alejandroarturousua@ufps.edu.co>
2022-12-01 23:19:58 -0500

    1.6

--- src/controllers/articuloController.js
+++ src/controllers/articuloController.js
@@ -34,16 +34,41 @@ function formatoArticulo(req, res){
 function registrarArticulo(req, res){
     
     const data = req.body
+    console.log(data);
     req.getConnection((err, conn) =>{
         conn.query(`SELECT * FROM articulo WHERE titulo = ?`, [data.titulo], (err, articulodata) => {
             if(articulodata.length > 0){
                 res.render('registrarArticulos', {error: 'Error: Article already exists !'})
-            } else{                    
-                    req.getConnection((err, conn) => {
-                        conn.query(`INSERT INTO articulo SET ?`, [data], (err, rows) =>{
-                            res.redirect('/RelArt')
+            } else{                   
+                    
+                        conn.query("SELECT id FROM usuario where email = ?",[data.correo], (err, rows) =>{
+                            console.log(rows);
+                            var values = {
+                                usuario_id: rows[0].id,
+                                titulo: data.titulo,
+                                autores: data.autores,
+                                citacion: data.citacion,
+                                pais: data.pais,
+                                ano: data.ano,
+                                palabras_clave: data.palabrasClave,
+                                url: data.url,
+                                resumen: data.resumen,
+                                conclusiones: data.conclusiones,
+                                notas: data.notas
+                            } 
+                            console.log(values);
+                            /*conn.query('INSERT INTO articulo SET ?', [values], (err, rows2) =>{
+                                console.log(rows2);
+                                res.redirect('/articulos')
+                            })*/
+                            
+                                conn.query('INSERT INTO articulo (usuario_id,titulo,autores,citacion,pais,ano,palabras_clave,url,resumen,conclusiones,notas) values ("'+rows[0].id+'","'+data.titulo+'","'+data.autores+'","'+data.citacion+'","'+data.pais+'","'+data.ano+'","'+data.palabrasClave+'","'+data.url+'","'+data.resumen+'","'+data.conclusiones+'","'+data.notas+'")', (err, rows) =>{
+                                    
+                                    res.redirect('/articulos')
+                                })
+                            
                         })
-                    })
+                        
             }
         })
     })
