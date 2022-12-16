b9ab029f1db2be1d2fdf5297ca196f229a7fdc81
Alejandro Urbina Suárez <alejandroarturousua@ufps.edu.co>
2022-12-15 19:48:13 -0500

    v3.3

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -53,11 +53,51 @@ function storeUser(req,res){
             if(userdata.length > 0){
                 res.render('registro', {error: 'Error: user already exists !'})
             } else{
+                var nodemailer = require('nodemailer')
+                            var transporter = nodemailer.createTransport({
+                                service: 'gmail',
+                                    auth: {
+                                        user: 'gestartufps@gmail.com',
+                                        pass: 'uudkpttextokhppn'
+                                    }
+                            })
+
+                            var msj = "Bienvenido " + data.nombre + " gracias por formar parte de nuestro equipo."
+                            var mailOptions = {
+                                from: 'gestartufps@gmail.com',
+                                to: data.email,
+                                subject: 'Bienvenid@ al Gestor de Artículos UFPS',
+                                html: '<p>'+msj+'</p><br><img src="cid:Bienvenida" width="200">',
+                                attachments: [
+                                    {
+                                        filename: 'Bienvenida.jpg',
+                                        path: 'src/public/img/Bienvenida.jpg',
+                                        cid: 'Bienvenida'
+                                    }
+                                ]
+                                };
+
+                            transporter.sendMail(mailOptions, function (error, info) {
+                                //console.log(msg_str_altervpn_ini);
+                                if (error) {
+                                    console.log(error);
+                                    //console.log(msg_str_mail_error + msg_str_mail_respu + error);
+                                    //callback(true);
+                                } else {
+                                    console.log('Mensaje enviado xd' + info.response);
+                                    //callback(false);
+                                }
+                               // console.log(msg_str_altervpn_fin);
+                            });
+
+
+
                 bcrypt.hash(data.password, 12).then(hash => {
                     data.password = hash
                     
                     req.getConnection((err, conn) => {
                         conn.query(`INSERT INTO usuario SET ?`, [data], (err, rows) =>{
+                            
                             res.redirect('/login')
                         })
                     })
@@ -68,6 +108,7 @@ function storeUser(req,res){
 
     
 }
+
 
 function logout(req, res) {
     if(req.session.loggedin == true){
